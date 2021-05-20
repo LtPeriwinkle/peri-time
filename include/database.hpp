04a76ad339b03ae3329402a122e90d3afd7196ff
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-05-20 16:30:28 -0700

    remove db.cpp

--- include/database.hpp
+++ include/database.hpp
@@ -7,6 +7,5 @@ struct sqlite3_delete {
   void operator() (sqlite3 *db) const {sqlite3_close_v2(db);};
 };
 using sqlite3_handle = std::unique_ptr<sqlite3, sqlite3_delete>;
-sqlite3_stmt *prepare_insert(int64_t uid, std::string tz);
 
 #endif
