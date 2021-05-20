de589dc4ebaf31c623a549f3571834a5a4223c08
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-05-20 15:37:25 -0700

    format

--- src/tz_client.cpp
+++ src/tz_client.cpp
@@ -24,7 +24,7 @@ void TzBotClient::onMessage(Message message) {
         bot_user = userCache.at(this->getID());
       } catch (std::out_of_range &e) {
         bot_user = this->getCurrentUser();
-      }  
+      }
       if (words[1] == "quit" &&
           (message.author.ID.number() == 658861212657909791)) {
         sendMessage(message.channelID, "bye");
@@ -36,52 +36,56 @@ void TzBotClient::onMessage(Message message) {
       } else if (words[1] == "set") {
         if (words.size() == 3) {
           try {
-            const date::time_zone *zone =
-                date::locate_zone(words[2]);
+            const date::time_zone *zone = date::locate_zone(words[2]);
             auto time =
                 date::make_zoned(zone, date::floor<std::chrono::seconds>(
                                            std::chrono::system_clock::now()));
             std::ostringstream zonetime;
             zonetime << "current time: " << time;
-            sqlite3_stmt* stmt;
-            sqlite3_prepare_v2(userDB.get(), set_tz_query.c_str(), set_tz_query.length(), &stmt, nullptr);
+            sqlite3_stmt *stmt;
+            sqlite3_prepare_v2(userDB.get(), set_tz_query.c_str(),
+                               set_tz_query.length(), &stmt, nullptr);
             sqlite3_bind_int64(stmt, 1, message.author.ID.number());
-            sqlite3_bind_text(stmt, 2, zone->name().c_str(), zone->name().length(), SQLITE_STATIC); 
+            sqlite3_bind_text(stmt, 2, zone->name().c_str(),
+                              zone->name().length(), SQLITE_STATIC);
             int rc = sqlite3_step(stmt);
             sqlite3_finalize(stmt);
             if (rc != SQLITE_DONE) {
               std::string sql_error(sqlite3_errmsg(userDB.get()));
               emb = Embed(err_embed("database error: " + sql_error, &bot_user));
             } else {
-              emb = Embed(gen_embed("Success",
-                                    "set to " + words[2] + ", " + zonetime.str(),
-                                    &bot_user));
+              emb = Embed(gen_embed(
+                  "Success", "set to " + words[2] + ", " + zonetime.str(),
+                  &bot_user));
             }
           } catch (std::runtime_error &error) {
             emb = Embed(err_embed(error.what(), &bot_user));
           }
         } else {
-          emb =
-              Embed(err_embed("Not enough arguments!", &bot_user));
+          emb = Embed(err_embed("Not enough arguments!", &bot_user));
         }
       } else if (words[1] == "get") {
-          if (words.size() == 2) {
-            int rc;
-            sqlite3_stmt *stmt;
-            sqlite3_prepare_v2(userDB.get(), get_tz_query.c_str(), get_tz_query.length(), &stmt, nullptr);
-            sqlite3_bind_int64(stmt, 1, message.author.ID.number());
-            rc = sqlite3_step(stmt);
-            if (rc != SQLITE_ROW) {
-              std::string sql_error(sqlite3_errmsg(userDB.get()));
-              emb = Embed(err_embed("database error: " + sql_error, &bot_user));
-            } else {
-              std::string s(reinterpret_cast<char const*>(sqlite3_column_text(stmt, 0)));
-              emb = Embed(gen_embed("Timezone set by " + message.author.username + "#" + message.author.discriminator, s, &bot_user));
-            }
-            sqlite3_finalize(stmt);
-          } else if (words.size() == 3) {
-            /*getter for other users will be here*/
+        if (words.size() == 2) {
+          int rc;
+          sqlite3_stmt *stmt;
+          sqlite3_prepare_v2(userDB.get(), get_tz_query.c_str(),
+                             get_tz_query.length(), &stmt, nullptr);
+          sqlite3_bind_int64(stmt, 1, message.author.ID.number());
+          rc = sqlite3_step(stmt);
+          if (rc != SQLITE_ROW) {
+            std::string sql_error(sqlite3_errmsg(userDB.get()));
+            emb = Embed(err_embed("database error: " + sql_error, &bot_user));
+          } else {
+            std::string s(
+                reinterpret_cast<char const *>(sqlite3_column_text(stmt, 0)));
+            emb = Embed(gen_embed("Timezone set by " + message.author.username +
+                                      "#" + message.author.discriminator,
+                                  s, &bot_user));
           }
+          sqlite3_finalize(stmt);
+        } else if (words.size() == 3) {
+          /*getter for other users will be here*/
+        }
       } else {
         emb = Embed(err_embed("Command not found", &bot_user));
       }
