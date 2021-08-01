e83c189d0b5f4bfac1fdb5a17d0e6101d755a20f
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-07-31 19:57:54 -0700

    remove using namespace

--- include/tz_client.hpp
+++ include/tz_client.hpp
@@ -4,18 +4,20 @@
 #include <sqlite3.h>
 #ifndef TZ_CLIENT_H
 #define TZ_CLIENT_H
-using namespace SleepyDiscord;
 
-class TzBotClient : public DiscordClient {
+class TzBotClient : public SleepyDiscord::DiscordClient {
 public:
-  using DiscordClient::DiscordClient;
-  void onMessage(Message message) override;
-  void onServer(Server server) override;
-  void onReady(Ready readyData) override;
+  using SleepyDiscord::DiscordClient::DiscordClient;
+  void onMessage(SleepyDiscord::Message message) override;
+  void onServer(SleepyDiscord::Server server) override;
+  void onReady(SleepyDiscord::Ready readyData) override;
 
 private:
-  std::unordered_map<Snowflake<User>::RawType, User> userCache;
-  std::unordered_map<Snowflake<Server>::RawType, Server> guildCache;
+  std::string doCommand(SleepyDiscord::Message *message);
+  std::string tzGet(std::vector<std::string> words, SleepyDiscord::Message *message);
+
+  std::unordered_map<SleepyDiscord::Snowflake<SleepyDiscord::User>::RawType, SleepyDiscord::User> userCache;
+  std::unordered_map<SleepyDiscord::Snowflake<SleepyDiscord::Server>::RawType, SleepyDiscord::Server> guildCache;
   std::string get_tz_query = "SELECT tz FROM tbl1 WHERE uid=?;";
   std::string set_tz_query = "INSERT OR REPLACE INTO tbl1(uid, tz) VALUES(?, ?);";
   sqlite3_handle userDB;
