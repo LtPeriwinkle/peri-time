a296ef00078d84fb542880f173d2e79db0cc49b6
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-07-31 19:18:51 -0700

    update embeds

--- src/embed_templates.cpp
+++ src/embed_templates.cpp
@@ -4,25 +4,25 @@
 #include <string>
 std::string gen_embed(std::string title, std::string description,
                       SleepyDiscord::User *sender) {
-  std::stringstream ss;
-  ss << R"({"title": ")" << title << R"(", "description": ")" << description
-     << R"(", "color": )" << 4293174
-     << R"(,"footer": {"text": ")" << sender->username << R"(", 
+    std::stringstream ss;
+    ss << R"({"title": ")" << title << R"(", "description": ")" << description
+       << R"(", "color": )" << 4293174 << R"(,"footer": {"text": ")"
+       << sender->username << R"(", 
      "icon_url": "https://cdn.discordapp.com/avatars/)"
-     << sender->ID.number() << "/" << sender->avatar << ".png"
-     << R"("}})";
-  return ss.str();
+       << sender->ID.number() << "/" << sender->avatar << ".png"
+       << R"("}})";
+    return ss.str();
 }
 
 std::string err_embed(std::string error, SleepyDiscord::User *sender) {
-  std::stringstream ss;
-  ss << R"({"title": "Error",)"
-     << R"( "description": ")" << error << R"(", "color": )" << 11089976
-     << R"(,"footer": {"text": ")" << sender->username << R"(", 
+    std::stringstream ss;
+    ss << R"({"title": "Error",)"
+       << R"( "description": ")" << error << R"(", "color": )" << 11089976
+       << R"(,"footer": {"text": ")" << sender->username << R"(", 
      "icon_url": "https://cdn.discordapp.com/avatars/)"
-     << sender->ID.number() << "/" << sender->avatar << ".png"
-     << R"("}})";
-  return ss.str();
+       << sender->ID.number() << "/" << sender->avatar << ".png"
+       << R"("}})";
+    return ss.str();
 }
 
 std::string help_embed(SleepyDiscord::User *sender) {
@@ -31,8 +31,11 @@ std::string help_embed(SleepyDiscord::User *sender) {
     "title": "Help",
     "color": 21402,
     "footer": {
-      "icon_url": "https://cdn.discordapp.com/avatars/)" << sender->ID.number() << "/" << sender->avatar << ".png" << R"(",
-      "text": ")" << sender->username << R"("
+      "icon_url": "https://cdn.discordapp.com/avatars/)"
+       << sender->ID.number() << "/" << sender->avatar << ".png"
+       << R"(",
+      "text": ")"
+       << sender->username << R"("
     },
     "fields": [
       {
@@ -44,18 +47,18 @@ std::string help_embed(SleepyDiscord::User *sender) {
         "value": "Set your timezone to <timezone>. Timezone list available [here](https://kevinnovak.github.io/Time-Zone-Picker/)."
       },
       {
-        "name": "`;tz get`",
-        "value": "Get the timezone that you set."
-      },
-      {
-        "name": "`;tz get <user>`",
-        "value": "Get the timezone set by <user>. You can either ping the user or type their full username/full server nick **exactly**."
+        "name": "`;tz get [<user>]`",
+        "value": "Get the timezone that you set, or the timezone of the specified user"
       },
       {
         "name": "`;tz set <user> <timezone>`",
         "value": "Available only to those with Admin, Kick, Ban, or Manage Nicknames permissions. Set the timezone for <user> to <timezone>. Timezone list available [here](https://kevinnovak.github.io/Time-Zone-Picker/). You can either ping the user or type their full username/full server nick **exactly**."
+      },
+      {
+        "name": "`;tz time [<user> | <timezone>]`"
+        "value": "Get the current time for <user> or in <timezone>."
       }
     ]
 })";
-  return ss.str();
+    return ss.str();
 }
